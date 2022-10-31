73d0835d447832ab46f5620ea96e0e94d8193e32
Yining <ynzhang23@gmail.com>
2022-10-31 11:57:53 +0800

    Create 16 rows of flex containers within containerZero

--- script.js
+++ script.js
@@ -7,5 +7,13 @@ document.body.appendChild(containerZero);
 containerZero.classList.toggle('containerZero');
 
 // create row box
+let containerRow = [];
+
+for (i = 0; i < 16; i++) {
+    containerRow[i] = document.createElement('div');
+    containerZero.appendChild(containerRow[i]);
+    containerRow[i].classList.toggle('containerRow');
+}
+
 // create grids * 16 in row box
 // repeat row box 16 times
