87ae04f30e7a5fff552ece44843bcc68167da465
Yining <ynzhang23@gmail.com>
2022-10-31 16:11:13 +0800

    Create input field for dimension of the canvas

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-// specify dimension = 16
-const dimension = 16;
+// create tool box
+let toolBox = document.createElement('div');
+toolBox.classList.toggle('toolBox'); 
+document.body.appendChild(toolBox);
+
+// create dimension adjustment container
+let containerDimension = document.createElement('div');
+containerDimension.classList.toggle('containerDimension');
+toolBox.appendChild(containerDimension);
+
+// create box for inputting dimension
+let dimensionInputBox = document.createElement('input');
+dimensionInputBox.classList.toggle('dimensionInput');
+containerDimension.appendChild(dimensionInputBox);
+
+// create button to receive input
+let dimensionBtn = document.createElement('button');
+dimensionBtn.classList.toggle('dimensionBtn');
+containerDimension.appendChild(dimensionBtn);
+
+dimensionBtn.textContent = "New Canvas";
+
+// change dimension value
+let dimension;
+dimensionBtn.addEventListener('click', () =>
+    dimension = document.querySelector('.dimensionBtn')
+)
 
 // create containing box
 let containerZero = document.createElement('div');
@@ -28,7 +53,6 @@ for (i = 0; i < 16; i++) {
 }
 
 // allow box to detect hover, changes its color on hover
-
 // add event listener to all boxes
 for (i = 0; i < 16; i++) {
     for (j = 0; j < 16; j++) {
