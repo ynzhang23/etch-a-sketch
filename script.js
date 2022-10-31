ab81d64c6cdebbda6f56b78f8b711f6ded86f652
Yining <ynzhang23@gmail.com>
2022-10-31 11:44:20 +0800

    Create outer container for grid and set its flexbox properties

---
+++ script.js
@@ -0,0 +1,11 @@
+// specify dimension = 16
+const dimension = 16;
+
+// create containing box
+let containerZero = document.createElement('div');
+document.body.appendChild(containerZero);
+containerZero.classList.toggle('containerZero');
+
+// create row box
+// create grids * 16 in row box
+// repeat row box 16 times
