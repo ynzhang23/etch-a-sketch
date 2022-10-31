114f9c68b4b8afd4a74d4de77b55c560127c5cee
Yining <ynzhang23@gmail.com>
2022-10-31 17:25:43 +0800

    Implement button to generate new canvas
    With the input value, new canvas is generated according to the new
    dimension.
    
    Upon pressing the button, the original canvas is deleted and a new one
    takes its place.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+// create largest container
+let containerBody = document.createElement('div');
+containerBody.classList.toggle('body');
+document.body.appendChild(containerBody);
+
 // create tool box
 let toolBox = document.createElement('div');
 toolBox.classList.toggle('toolBox'); 
-document.body.appendChild(toolBox);
+containerBody.appendChild(toolBox);
 
 // create dimension adjustment container
 let containerDimension = document.createElement('div');
@@ -17,26 +22,20 @@ containerDimension.appendChild(dimensionInputBox);
 let dimensionBtn = document.createElement('button');
 dimensionBtn.classList.toggle('dimensionBtn');
 containerDimension.appendChild(dimensionBtn);
-
 dimensionBtn.textContent = "New Canvas";
 
-// change dimension value
-let dimension;
-dimensionBtn.addEventListener('click', () =>
-    dimension = document.querySelector('.dimensionBtn')
-)
-
+// ================================================================================================
 // create containing box
-let containerZero = document.createElement('div');
-document.body.appendChild(containerZero);
-containerZero.classList.toggle('containerZero');
+let containerCanvas = document.createElement('div');
+containerBody.appendChild(containerCanvas);
+containerCanvas.classList.toggle('containerCanvas');
 
 // create row box
 let containerRow = [];
 
 for (i = 0; i < 16; i++) {
     containerRow[i] = document.createElement('div');
-    containerZero.appendChild(containerRow[i]);
+    containerCanvas.appendChild(containerRow[i]);
     containerRow[i].classList.toggle('containerRow' + i);
     containerRow[i].classList.toggle('row');
 }
@@ -63,4 +62,55 @@ for (i = 0; i < 16; i++) {
 // adds an extra class on hover
 function addHover(e) {
     e.target.classList.add('hover');
+}
+
+// change dimension value
+dimensionBtn.addEventListener('click', newCanvas);
+
+function newCanvas() {
+    // clear old canvas
+    var x = document.getElementsByClassName('containerCanvas')[0];
+    x.parentNode.removeChild(x);
+
+    // create new canvas
+    let dimension = document.querySelector('.dimensionInput').value;
+
+    // create containing box
+    let containerCanvas = document.createElement('div');
+    containerBody.appendChild(containerCanvas);
+    containerCanvas.classList.toggle('containerCanvas');
+
+    // create row box
+    let containerRow = [];
+
+    for (i = 0; i < dimension; i++) {
+        containerRow[i] = document.createElement('div');
+        containerCanvas.appendChild(containerRow[i]);
+        containerRow[i].classList.toggle('containerRow' + i);
+        containerRow[i].classList.toggle('row');
+    }
+
+    // create 16 boxes per row
+    // box 3 of row 5 = containerRow[5][3]
+    for (i = 0; i < dimension; i++) {
+        for (j = 0; j < dimension; j++) {
+            containerRow[i][j] = document.createElement('div');
+            containerRow[i].appendChild(containerRow[i][j]);
+            containerRow[i][j].classList.toggle('row' + i + 'box' + j);
+            containerRow[i][j].classList.toggle('box');  
+        }
+    }
+
+    // allow box to detect hover, changes its color on hover
+    // add event listener to all boxes
+    for (i = 0; i < dimension; i++) {
+        for (j = 0; j < dimension; j++) {
+            containerRow[i][j].addEventListener('mouseover', addHover)
+        }
+    }
+
+    // adds an extra class on hover
+    function addHover(e) {
+        e.target.classList.add('hover');
+    }
 }
